0234e62c84db2bced6b33368179f8ff218d7604a
kozakevich13 <63108530+kozakevich13@users.noreply.github.com>
2023-08-05 10:32:55 +0300

    add component details

---
+++ src/components/details/Details.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Details.css";
+
+const Details: React.FC = () => (
+  <div className="details-container">
+    <div className="details-textContainer">
+      <div className="details-text">
+        Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.
+      </div>
+    </div>
+    <div className="details-titleContainer">
+      <div className="details-title">mostly tastes with freshes</div>
+      <div className="details-wrapperButton">
+        <div className="details-button">TESTE IT</div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Details;
